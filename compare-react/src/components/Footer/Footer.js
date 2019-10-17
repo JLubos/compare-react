c81fe5a8a8539b6685fe8cbe3910dba8bf5fdb5e
Justin <juslubos@yahoo.com>
2019-10-17 00:09:53 -0500

    Second commit 10-17-9 @ 12:09am

---
+++ compare-react/src/components/Footer/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+/*import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+*/
+
+
+const Footer = () => {
+
+    return (
+      <div className="App">
+      My Footer
+      </div>
+    );
+  };
+
+export default Footer;
---
+++ compare-react/src/components/Header/Header.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+
+
+
+
+/*import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+*/
+
+
+class Header extends Component {
+
+  render() {
+    return (
+      <div className="App">
+      My Header
+      </div>
+    );
+  }
+}
+
+
+export default Header;
